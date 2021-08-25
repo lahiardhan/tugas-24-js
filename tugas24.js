10ce2ab31e87b8a745fcf882a55b0d56adccac70
lahiardhan <lahia.ardhanm@gmail.com>
2021-08-25 17:08:42 +0700

    Tugas 24 JS
    Tugas ini saya serahkan kepada Tim Niomic

---
+++ tugas24.js
@@ -0,0 +1,4 @@
+const angka = [32, 33, 16, 40, 9, 7, 11, 2, 44, 1, 66];
+console.log('Sebelumnya : ',angka.join(","));
+console.log('Ascending : ',angka.sort().join(","));
+console.log('Descending : ',angka.sort().reverse().join(","));
